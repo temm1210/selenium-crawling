f15689131eb97bebbb17b9d036a3e8fcf1a1da3f
temm1210 <30516609+temm1210@users.noreply.github.com>
2018-10-08 17:50:43 +0900

    Update connect-to-keeper.js

--- server/socket-client/connect-to-keeper.js
+++ server/socket-client/connect-to-keeper.js
@@ -1,19 +1,23 @@
 var KeeperInfo = require('../server-info/server-info').NodeKeeper;
 var socket = require('socket.io-client')(`http://localhost:${KeeperInfo.port}`);
 
+
 module.exports = function(serverInfo, onReceive) {
     socket.on('connect', () => {
         console.log(`---${serverInfo.name} connect to server: ${KeeperInfo.name}:${KeeperInfo.port})`);
         socket.emit('message', serverInfo);
     });
     
+    
     socket.on('message',(microNodes) => {  
         onReceive(microNodes);
     });
+    
 
     socket.on('disconnect', (reason) => {
         console.log(`---Client Close..${reason}`);
     });
+    
     
     socket.on('reconnect_attempt', (number) => console.log(`###  RECONNECT TO KEEPER ${number} times`));  
 }
